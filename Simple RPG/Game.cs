0fbf63e032ff4433e95450761b30496c0b0775d2
Ryan LaCombe <ryanalacombe@gmail.com>
2019-08-21 13:15:21 -0500

    Added multiple spells: fire, ice, thunder, and heal. Compressed code further

--- Simple RPG/Game.cs
+++ Simple RPG/Game.cs
@@ -10,8 +10,14 @@ namespace Simple_RPG
     {
         string playerName = "";
         int playerHealth = 100;
+        int playerMaxHealth = 100;
         int playerMana = 100;
         int playerExp = 0;
+        int playerDamage = 10;
+        int fireDamage = 15;
+        int iceDamage = 20;
+        int thunderDamage = 35;
+        int healDamage = 50;
         public void Start()
         {
 
@@ -24,7 +30,7 @@ namespace Simple_RPG
             while(alive && monsterRemaining > 0)
             {
                 Console.WriteLine("There are " + monsterRemaining + " Carbuncles remaining.");
-                alive = Encounter(15);
+                alive = Encounter(20, 15);
                 monsterRemaining--;
             }
             
@@ -39,55 +45,153 @@ namespace Simple_RPG
             Console.Write("Welcome, " + playerName + "! \n");
         }
 
-        bool Encounter(int monsterDamage)
+        bool Encounter(int monsterHealth, int monsterDamage)
         {
             //Monster Encounter
             Console.WriteLine("");
             Console.WriteLine("A Carbuncle blocks your way!");
 
             string action = "";
-            Console.WriteLine("What is thy action? (fight/spell/flee) ");
-            action = Console.ReadLine();
-            if (action == "fight" || action == "Fight")
+            bool survived = true;
+            while (playerHealth > 0 && monsterHealth > 0)
             {
-                //monster attacks
-                Console.WriteLine("The Carbuncle strikes!");
-                Console.WriteLine(playerName + " takes " + monsterDamage + " damage!");
-                playerHealth = playerHealth - monsterDamage;
-                Console.WriteLine(playerName + " has " + playerHealth + " life remaining");
-                if (playerHealth <= 0)
+
+                Console.WriteLine("What is thy action? (fight/spell/flee) ");
+                action = Console.ReadLine();
+                if (action == "fight" || action == "Fight")
                 {
-                    //player defear
-                    Console.WriteLine(playerName + " has fallen! The world is ruled be Darkness!");
-                    return false;
+                    survived = Fight(ref monsterHealth, ref monsterDamage);
+                    if (!survived)
+                    {
+                        return false;
+                    }
                 }
-                //player attacks
-                Console.WriteLine(playerName + " strikes!");
-                Console.WriteLine("The Carbuncle is defeated!");
-                playerExp = playerExp + 10;
-                Console.WriteLine(playerName + " gained 10 experience!");
+                
+                else if (action == "spell" || action == "Spell")
+                {
+                    survived = Spell(ref monsterHealth, ref monsterDamage);
+                    if(!survived)
+                    {
+                        return false;
+                    }
+                }
+                else if (action == "flee" || action == "Flee")
+                {
+
+                    //escape
+                    Console.WriteLine(playerName + " managed to escape!");
+                }
+            }
+            return survived;
+        }
+        bool Fight(ref int monsterHealth, ref int monsterDamage)
+        {
+            //monster attacks
+            Console.WriteLine("The Carbuncle strikes!");
+            Console.WriteLine(playerName + " takes " + monsterDamage + " damage!");
+            playerHealth = playerHealth - monsterDamage;
+            Console.WriteLine(playerName + " has " + playerHealth + " life remaining!");
+            if (playerHealth <= 0)
+            {
+                //player defeat
+                Console.WriteLine(playerName + " has fallen! The world is ruled by Darkness!");
+                return false;
+            }
+            //player attacks
+            Console.WriteLine(playerName + " strikes!");
+            Console.WriteLine("The Carbuncle takes " + playerDamage + " damage!");
+            monsterHealth -= playerDamage;
+            Console.WriteLine("The Carbuncle has " + monsterHealth + " life remaining!");
+            if (monsterHealth <= 0)
+            {
+                //monster defeat
+                Console.WriteLine("The Carbuncle has been defeated!");
+                return true;
+            }
+            return true;
+        }       
+
+        bool Flee()
+        {
+            return true;
+        }
+
+        bool Spell(ref int monsterHealth, ref int monsterDamage)
+        {
+            //monster attacks
+            Console.WriteLine("The Carbuncle strikes!");
+            Console.WriteLine(playerName + " takes " + monsterDamage + " damage!");
+            playerHealth = playerHealth - monsterDamage;
+            Console.WriteLine(playerName + " has " + playerHealth + " life remaining!");
+            if (playerHealth <= 0)
+
+            {
+                //player defeat
+                Console.WriteLine(playerName + " has fallen! The world is ruled by Darkness!");
+                return false;
 
             }
-            else if (action == "spell" || action == "Spell")
+            //player chooses spell
+            string spellAction = "";
+            Console.WriteLine("Which spell do you cast? (fire/ice/thunder/heal)");
+            spellAction = Console.ReadLine();
+            //player uses spell
+            if (spellAction == "fire" || spellAction == "Fire")
             {
-                //monster attacks
-                Console.WriteLine("The Carbuncle strikes!");
-                Console.WriteLine(playerName + " takes " + monsterDamage + " damage!");
-                playerHealth = playerHealth - monsterDamage;
-                Console.WriteLine(playerName + "has " + playerHealth + " life remaining");
-                //player uses spell
-                Console.WriteLine(playerName + " casts Fire!");
+                Console.WriteLine(playerName + " casts fire!");
+                Console.WriteLine("The Carbuncle takes " + fireDamage + " damage!");
+                monsterHealth -= fireDamage;
+                Console.WriteLine("The Carbuncle has " + monsterHealth + " life remaining!");
                 playerMana = playerMana - 5;
-                Console.WriteLine("The Carbuncle is defeated!");
-                playerExp = playerExp + 10;
-                Console.WriteLine(playerName + " gained 10 experience!");
+                if (monsterHealth <= 0)
+                {
+                    //monster defeat
+                    Console.WriteLine("The Carbuncle has been defeated!");
+                    return true;
+                }
             }
-            else if (action == "flee" || action == "Flee")
+            else if (spellAction == "ice" || spellAction == "Ice")
             {
-
-                //escape
-                Console.WriteLine(playerName + " managed to escape!");
+                Console.WriteLine(playerName + " casts ice!");
+                Console.WriteLine("The Carbuncle takes " + iceDamage + " damage!");
+                monsterHealth -= iceDamage;
+                Console.WriteLine("The Carbuncle has " + monsterHealth + " life remaining!");
+                playerMana = playerMana - 10;
+                if (monsterHealth <= 0)
+                {
+                    //monster defeat
+                    Console.WriteLine("The Carbuncle has been defeated!");
+                    return true;
+                }
             }
+            else if (spellAction == "thunder" || spellAction == "Thunder")
+            {
+                Console.WriteLine(playerName + " casts thunder!");
+                Console.WriteLine("The Carbuncle takes " + thunderDamage + " damage!");
+                monsterHealth -= thunderDamage;
+                Console.WriteLine("The Carbuncle has " + monsterHealth + " life remaining!");
+                playerMana = playerMana - 15;
+                if (monsterHealth <= 0)
+                {
+                    //monster defeat
+                    Console.WriteLine("The Carbuncle has been defeated!");
+                    return true;
+                }
+            } 
+            else if (spellAction == "heal" || spellAction == "Heal")
+            {
+                Console.WriteLine(playerName + " casts heal!");
+                if (playerHealth > 0 && playerHealth < playerMaxHealth)
+                {
+                    playerHealth = playerHealth + healDamage;
+                    Console.WriteLine(playerName + " has restored " + healDamage + " health!");
+                    Console.WriteLine(playerName + " now has " + playerHealth + "reamining");
+                }
+                else if (playerHealth <= 0 || playerHealth >= playerMaxHealth)
+                {
+                    Console.WriteLine(playerName + "'s spell fizzles!");
+                }
+            }         
             return true;
         }
     }
