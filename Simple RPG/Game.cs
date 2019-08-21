5958383634cb2e70b26a8d864d2d1c63d228b7a4
Ryan LaCombe <ryanalacombe@gmail.com>
2019-08-21 09:50:47 -0500

    Added a basic monster encounter with normal attack, basic spell, and escape option.

--- Simple RPG/Game.cs
+++ Simple RPG/Game.cs
@@ -10,7 +10,59 @@ namespace Simple_RPG
     {
         public void Start()
         {
-            Console.Write("Welcome! \n");
+            string playerName = "";
+            int playerHealth = 100;
+            int playerMana = 100;
+            int playerExp = 0;
+
+            //Welcome player & asks name
+            Console.WriteLine("What is thy name? ");
+            playerName = Console.ReadLine();
+            Console.Write("Welcome, " + playerName + "! \n");
+
+            //Monster Encounter
+            int monsterDamage = 15;
+            Console.WriteLine("");
+            Console.WriteLine("A Carbuncle blocks your way!");
+
+            string action = "";
+            Console.WriteLine("What is thy action? (fight/spell/flee) ");
+            action = Console.ReadLine();
+            if(action == "fight" || action == "Fight")
+            {
+                //monster attacks
+                Console.WriteLine("The Carbuncle strikes!");
+                Console.WriteLine(playerName + " takes " + monsterDamage + " damage!");
+                playerHealth = playerHealth - monsterDamage;
+                Console.WriteLine(playerName + "has " + playerHealth + " life remaining");
+                //player attacks
+                Console.WriteLine(playerName + " strikes!");
+                Console.WriteLine("The Carbuncle is defeated!");
+                playerExp = playerExp + 10;
+                Console.WriteLine(playerName + " gained " + playerExp + " experience!");
+
+            }
+            else if(action == "spell" || action == "Spell")
+            {
+                //monster attacks
+                Console.WriteLine("The Carbuncle strikes!");
+                Console.WriteLine(playerName + " takes " + monsterDamage + " damage!");
+                playerHealth = playerHealth - monsterDamage;
+                Console.WriteLine(playerName + "has " + playerHealth + " life remaining");
+                //player uses spell
+                Console.WriteLine(playerName + " casts Fire!");
+                playerMana = playerMana - 5;
+                Console.WriteLine("The Carbuncle is defeated!");
+                playerExp = playerExp + 10;
+                Console.WriteLine(playerName + " gained " + playerExp + " experience!");
+            }
+            else if(action == "flee" || action == "Flee")
+            {
+
+                //escape
+                Console.WriteLine(playerName + " managed to escape!");
+            }
+
             Console.ReadKey();
         }
     }
