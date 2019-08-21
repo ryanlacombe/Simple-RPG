3bf78c2c002801b3a6a63c087a2422662421f81b
Ryan LaCombe <s198023@students.aie.edu>
2019-08-21 09:14:54 -0500

    Add project files.

---
+++ Simple RPG/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_RPG
+{
+    class Game
+    {
+        public void Start()
+        {
+            Console.Write("Welcome! \n");
+            Console.ReadKey();
+        }
+    }
+}
