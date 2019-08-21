c357d8e1a6f3f2e4a1e3cf63bbf2498bca7e6b90
Ryan LaCombe <ryanalacombe@gmail.com>
2019-08-21 10:46:00 -0500

    Added multiple encounters and compressed code

--- Simple RPG/Game.cs
+++ Simple RPG/Game.cs
@@ -8,41 +8,67 @@ namespace Simple_RPG
 {
     class Game
     {
+        string playerName = "";
+        int playerHealth = 100;
+        int playerMana = 100;
+        int playerExp = 0;
         public void Start()
         {
-            string playerName = "";
-            int playerHealth = 100;
-            int playerMana = 100;
-            int playerExp = 0;
 
+            Welcome();
+
+            int monsterRemaining = 5;
+
+            bool alive = true;
+
+            while(alive && monsterRemaining > 0)
+            {
+                Console.WriteLine("There are " + monsterRemaining + " Carbuncles remaining.");
+                alive = Encounter(15);
+                monsterRemaining--;
+            }
+            
+
+            Console.ReadKey();
+        }
+        void Welcome()
+        {
             //Welcome player & asks name
             Console.WriteLine("What is thy name? ");
             playerName = Console.ReadLine();
             Console.Write("Welcome, " + playerName + "! \n");
+        }
 
+        bool Encounter(int monsterDamage)
+        {
             //Monster Encounter
-            int monsterDamage = 15;
             Console.WriteLine("");
             Console.WriteLine("A Carbuncle blocks your way!");
 
             string action = "";
             Console.WriteLine("What is thy action? (fight/spell/flee) ");
             action = Console.ReadLine();
-            if(action == "fight" || action == "Fight")
+            if (action == "fight" || action == "Fight")
             {
                 //monster attacks
                 Console.WriteLine("The Carbuncle strikes!");
                 Console.WriteLine(playerName + " takes " + monsterDamage + " damage!");
                 playerHealth = playerHealth - monsterDamage;
-                Console.WriteLine(playerName + "has " + playerHealth + " life remaining");
+                Console.WriteLine(playerName + " has " + playerHealth + " life remaining");
+                if (playerHealth <= 0)
+                {
+                    //player defear
+                    Console.WriteLine(playerName + " has fallen! The world is ruled be Darkness!");
+                    return false;
+                }
                 //player attacks
                 Console.WriteLine(playerName + " strikes!");
                 Console.WriteLine("The Carbuncle is defeated!");
                 playerExp = playerExp + 10;
-                Console.WriteLine(playerName + " gained " + playerExp + " experience!");
+                Console.WriteLine(playerName + " gained 10 experience!");
 
             }
-            else if(action == "spell" || action == "Spell")
+            else if (action == "spell" || action == "Spell")
             {
                 //monster attacks
                 Console.WriteLine("The Carbuncle strikes!");
@@ -54,16 +80,15 @@ namespace Simple_RPG
                 playerMana = playerMana - 5;
                 Console.WriteLine("The Carbuncle is defeated!");
                 playerExp = playerExp + 10;
-                Console.WriteLine(playerName + " gained " + playerExp + " experience!");
+                Console.WriteLine(playerName + " gained 10 experience!");
             }
-            else if(action == "flee" || action == "Flee")
+            else if (action == "flee" || action == "Flee")
             {
 
                 //escape
                 Console.WriteLine(playerName + " managed to escape!");
             }
-
-            Console.ReadKey();
+            return true;
         }
     }
 }
